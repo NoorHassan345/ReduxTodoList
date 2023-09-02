93752c68cbe82fea5252126dc4984c37805533a0
noor hassan <noorhassan345@gmail.com>
2023-09-02 22:00:52 +0500

    Todo

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Entertask from "./features/entertask/Entertask";
 
-
 import List from "./features/list/List";
 
-
 function App() {
-  
   return (
     <>
- 
       <Entertask />
-    <List/>
+      <List />
     </>
   );
 }
